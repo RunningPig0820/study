675f3b977aa7035cf4e92fa85341b6d8c7f36979
zhangmin <1269917591@qq.com>
2019-08-13 10:26:49 +0800

    添加总结方法

---
+++ summaryFramework/README.md
@@ -0,0 +1,2 @@
+# summaryFarmework
+总结工作中遇到的好的处理方法
---
+++ summaryFramework/src/main/java/com/taosha/summaryframework/SummaryFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.taosha.summaryframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryFrameworkApplication.class, args);
+	}
+
+}
